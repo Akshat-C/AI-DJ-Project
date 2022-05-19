d396e2c58f466ab5f4114fc7909f0701926f0925
Akshat-C <85225437+Akshat-C@users.noreply.github.com>
2022-05-19 19:44:32 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-song = "";
+song_speed = 0;
+song_volume = 0;
+lwx = "";
+lwy = "";
+rwx = "";
+rwy = "";
+p_song = null;
 
 function preload()
 {
@@ -12,6 +18,26 @@ function setup()
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+    p_song = Math.floor(Math.random()*2);
+}
+
+function modelLoaded()
+{
+    console.log("PoseNet is loaded");
+}
+
+function gotPoses(results)
+{
+    if (results > 0)
+    {
+        console.log(results);
+        lwx = results[0].pose.leftWrist.x;
+        lwy = results[0].pose.leftWrist.y;
+        rwx = results[0].pose.rightWrist.x;
+        rwy = results[0].pose.rightWrist.y;
+    }
 }
 
 function draw()
@@ -21,14 +47,48 @@ function draw()
 
 function play1()
 {
-    p_song = Math.floor(Math.random()*2);
     if (p_song == 0)
     {
+        
         song1.play();
-        document.getElementById("song_name").innerHTML = "Harry Potter Theme Song"
+        document.getElementById("song_name").innerHTML = "Harry Potter Theme Song";
     } else if (p_song == 1)
     {
         song2.play();
-        document.getElementById("song_name").innerHTML = "Peter Pan Song"
+        document.getElementById("song_name").innerHTML = "Peter Pan Song";
     }
+}
+
+function change_speed()
+{
+    song_speed = document.getElementById("speed").value;
+    if (p_song == 0)
+    {
+        song1.playbackRate = song_speed;
+        song1.stop();
+        song1.play();
+    } else if (p_song == 1 )
+    {
+        song2.playbackRate = song_speed;
+        song2.stop();
+        song2.play();
+    }
+    document.getElementById("spe").innerHTML = song_speed
+}
+
+function change_volume()
+{
+    song_volume = document.getElementById("vol").value;
+    if (p_song == 0)
+    {
+        song1.playbackRate = song_speed;
+        song1.stop();
+        song1.play();
+    } else if (p_song == 1 )
+    {
+        song2.playbackRate = song_speed;
+        song2.stop()
+        song2.play();
+    }
+    document.getElementById("volume").innerHTML = song_volume
 }
